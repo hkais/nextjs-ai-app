4c3d5150efbb59d111db53023ed71413dd2de6c0
Hamady Kais <hkais0@gmail.com>
2025-08-14 00:19:56 +0300

    After Video 11 - Analyze Images

---
+++ src/app/api/multi-modal-chat/route.ts
@@ -0,0 +1,18 @@
+import { UIMessage, streamText, convertToModelMessages } from "ai";
+import { openai } from "@ai-sdk/openai";
+
+export async function POST(req: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await req.json();
+
+    const result = streamText({
+      model: openai("gpt-4.1-nano"),
+      messages: convertToModelMessages(messages),
+    });
+
+    return result.toUIMessageStreamResponse();
+  } catch (error) {
+    console.log("Error streamin chat completion", error);
+    return new Response("Failed to stream chat completion", { status: 500 });
+  }
+}
